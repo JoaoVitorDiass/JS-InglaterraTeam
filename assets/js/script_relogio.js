f0cc096aa0ed82055dc5893e234c213ef16536cc
Gabriel Barbosa <22gabrielbarbosa22@gmail.com>
2022-10-05 20:07:51 -0300

    Correção do Horário

--- assets/js/script_relogio.js
+++ assets/js/script_relogio.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "11/21/",
+        dayMonth = "11/20/",
         birthday = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
